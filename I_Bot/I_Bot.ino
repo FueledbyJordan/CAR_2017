c879c6b690c07fbedfb222ae7d957e6405859d8f
Jordan <Murrayjordan96@gmail.com>
2017-03-30 10:51:55 -0400

    Add logic for breaking an infinite loop.

--- I_Bot/I_Bot.ino
+++ I_Bot/I_Bot.ino
@@ -11,7 +11,7 @@ void setup(){
     motorInit();
     sensor.init();
     initMaster();
-    
+
 }
 
 void loop(){
@@ -19,41 +19,40 @@ void loop(){
   while(!getStartSwitch()){
     delay(100);
   }
-  
-  
+
+
   startSideBots();
 
   //SEND START MESSAGE TO SIDEBOTS
   move(-100,0,0);
-  while(sensor.getFrontLeftDistance() > 5){
+  while(sensor.getFrontLeftDistance() >= 5){
   }
   move(0,0,0);
-  
+
   //STAGE TWO
   for (int i = 0; i < 5; i++){
-    getPulse();
+    getPulse(); //add a count and exit when count gets to a number using OR
     swing();
   }
-  
+
   //REQUEST CODE FROM SB1
-  while(getNetworkCode() == ""){
+  while(getNetworkCode() == ""){ //add a count and exit when count gets to a number using OR
       masterRequest(SB1_ADDRESS);
       delay(100);
   }
-    
+
   //SEND CODE TO SB2
   sendCode();
 
   //REQUEST SB2 FINISHED
-  while(!getsb2Done()){
+  while(!getsb2Done()){ //add a count and exit when count gets to a number using OR
     masterRequest(SB2_ADDRESS);
     delay(100);
   }
 
   fire();
-  
+
   while(1){
 
   }
 }
-
